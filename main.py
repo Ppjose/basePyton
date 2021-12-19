9624e34cd2c8564eedf2f3419b841a6afea26980
ppjose <josepalomare@gmail.com>
2021-12-19 20:47:53 +0100

    codificar palabras

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
-# This is a sample Python script.
 # Python3 code to demonstrate working of
 # Convert numeric words to numbers
 # Using join() + split()
 
 help_dict = {
-	'a': '1',
-	'b': '2',
-	'c': '3',
-	'd': '4',
-	'e': '5',
-	'f': '6',
-	'g': '7',
-	'h': '8',
-	'i': '9',
-	'j': '0'
+	'one': '1',
+	'two': '2',
+	'three': '3',
+	'four': '4',
+	'five': '5',
+	'six': '6',
+	'seven': '7',
+	'eight': '8',
+	'nine': '9',
+	'zero': '0'
 }
 
 # initializing string
-test_str = "a b c d e f "
+test_str = "zero four zero one"
 
 # printing original string
 print("The original string is : " + test_str)
 
 # Convert numeric words to numbers
 # Using join() + split()
-res = ''.join(help_dict[ele] for ele in test_str.split())
-x = int(res)
+res = ''.join(help_dict[ele]
+for ele in test_str.split())
+
 # printing result
-print(x+1000)
+print("The string after performing replace : " + res)
+
+
+#x = int(res)
+# printing result
+#print(x+1000)
